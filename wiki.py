01eaebcb3320d75dd695937d58cab46a8f48707b
Jinxiao <jinxiao.wang@my.jcu.edu.au>
2022-01-25 10:34:10 +0800

    Initial commit

---
+++ wiki.py
@@ -0,0 +1,20 @@
+import json
+import urllib2
+import sys
+
+
+title = " ".join(sys.argv[1:])
+title = urllib2.quote(title)
+try:
+    json_data = json.load(urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=extracts&exintro=&explaintext=&rvprop=content&format=json"));
+    try:
+        pages = json_data["query"]["pages"]
+        page_key = pages.keys()[0];
+        text =  pages[page_key]["extract"]
+        print "====="
+        print text
+        print "====="
+    except KeyError:
+        print "That page doesn't seem to exist, try changing your query!"
+except urllib2.URLError:
+    print "There was an error when opening the URL :( \nYou may not be connected to the interwebs!"
